f0e2047ba724139eb1347117812505ec5b6c507f
fanimu <fanimu7@gmail.com>
2023-12-20 18:48:28 +0700

    resolve icons banner

--- src/components/banner/Banner.js
+++ src/components/banner/Banner.js
@@ -4,7 +4,7 @@ import RightBanner from './RightBanner';
 
 const Banner = () => {
   return (
-    <section id="home" className="w-full pt-10 pb-20 flex items-center border-b-[1px] font-titleFont border-b-black">
+    <section id="home" className="w-full pt-10 pb-20 flex flex-col lgl:flex-row items-center border-b-[1px] font-titleFont border-b-black">
       <LeftBanner />
       <RightBanner />
     </section>
--- src/components/banner/LeftBanner.js
+++ src/components/banner/LeftBanner.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { useTypewriter, Cursor } from 'react-simple-typewriter';
+import { FaFacebookF, FaTwitter, FaLinkedinIn, FaReact } from 'react-icons/fa';
+import { SiTailwindcss, SiFigma, SiNextdotjs } from 'react-icons/si';
 
 const LeftBanner = () => {
   const [text] = useTypewriter({
@@ -12,7 +14,7 @@ const LeftBanner = () => {
   return (
     <div className="w-full lgl:w-1/2 flex flex-col gap-20">
       <div className="flex flex-col gap-5">
-        <h4 className=" text-lg font-normal">WELCOME TO MY WORLD</h4>
+        <h4 className="text-lg font-normal">WELCOME TO MY WORLD</h4>
         <h1 className="text-6xl font-bold text-white">
           Hi, I'm <span className="text-designColor capitalize">Fani Mulyana</span>
         </h1>
@@ -23,6 +25,39 @@ const LeftBanner = () => {
         <p className="text-base font-bodyFont leading-6 tracking-wide">
           I use animation as a third dimension by which to simplify experiences and kuiding thro each and every interaction. I'm not adding motion just to spruce things up, but doing it in ways that.
         </p>
+      </div>
+      <div className="flex justify-between">
+        <div>
+          <h2 className="text-base uppercase font-titleFont mb-4">Find me in</h2>
+          <div className="flex gap-4">
+            <span className="bannerIcon">
+              <FaFacebookF />
+            </span>
+            <span className="bannerIcon">
+              <FaTwitter />
+            </span>
+            <span className="bannerIcon">
+              <FaLinkedinIn />
+            </span>
+          </div>
+        </div>
+        <div>
+          <h2 className="text-base uppercase font-titleFont mb-4">Best Skill on</h2>
+          <div className="flex gap-4">
+            <span className="bannerIcon">
+              <FaReact />
+            </span>
+            <span className="bannerIcon">
+              <SiNextdotjs />
+            </span>
+            <span className="bannerIcon">
+              <SiTailwindcss />
+            </span>
+            <span className="bannerIcon">
+              <SiFigma />
+            </span>
+          </div>
+        </div>
       </div>
     </div>
   );
